b00d7178f555ae7291e57563e651351851c5e758
ksls2 <kelosori@espol.edu.ec>
2022-10-14 23:15:09 -0500

    first commit

---
+++ tarea#2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('Welcome to software for petroleum engineer')
+st.header("This going to be the first part of the project")
+st.write('This app is going to be used as the kickoff lecture of this subject')
+
